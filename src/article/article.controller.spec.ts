79a864c21ee0c0b0ae2c0646f0b26bcd39c4ed7c
Yevhen Kashkarov <kea.riland@gmail.com>
2022-09-22 18:23:14 +0300

    crud for articles

--- src/article/article.controller.spec.ts
+++ src/article/article.controller.spec.ts
@@ -4,6 +4,7 @@ import { ArticleService } from './article.service';
 
 describe('ArticleController', () => {
   let controller: ArticleController;
+  let service: ArticleService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,7 +19,20 @@ describe('ArticleController', () => {
     expect(controller).toBeDefined();
   });
 
-  it('GET list of articles"', () => {
-    expect(controller.getAll()).toHaveLength(1);
+  it('GET list of articles"', async () => {
+    const tCase = await controller.getAll();
+    expect(tCase).toBe(1);
   });
+  //
+  // it('GET single article"', () => {
+  //   expect(controller.getOne()).toHaveLength(1);
+  // });
+  //
+  // it('Create article"', () => {
+  //   expect(controller.create()).toHaveLength(1);
+  // });
+  //
+  // it('Update article"', () => {
+  //   expect(controller.update()).toHaveLength(1);
+  // });
 });
